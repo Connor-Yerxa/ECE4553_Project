b552237b705fc8e1acb4a44eeb59732bbc1f0de7
Connor-Yerxa <cyerxa@unb.ca>
2025-11-12 17:58:16 -0400

    Initialize Repo

---
+++ Train_emg.bat
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+python transduction_model.py --pretrained_wavenet_model "./models/wavenet_model/wavenet_model.pt" --output_directory "./models/transduction_model/" --voiced_data_directories "./emg_data/voiced_parallel_data,./emg_data/nonparallel_data" --silent_data_directories "./emg_data/silent_parallel_data"
---
+++ asr.py
@@ -0,0 +1,30 @@
+import os
+
+import whisper
+import jiwer
+import soundfile as sf
+import numpy as np
+from unidecode import unidecode
+
+def evaluate(testset, audio_directory, model_size="base"):
+    model = whisper.load_model(model_size)
+    predictions = []
+    targets = []
+    for i, datapoint in enumerate(testset):
+        audio_path = os.path.join(audio_directory,f'example_output_{i}.wav')
+        results = model.predict(audio_path)
+        text = results["text"]
+        # audio, rate = sf.read(os.path.join(audio_directory,f'example_output_{i}.wav'))
+        # assert rate == model.sampleRate(), 'wrong sample rate'
+        # audio_int16 = (audio*(2**15)).astype(np.int16)
+        # text = model.stt(audio_int16)
+        predictions.append(text)
+        target_text = unidecode(datapoint['text'])
+        targets.append(target_text)
+
+    transformation = jiwer.Compose([jiwer.RemovePunctuation(), jiwer.ToLowerCase()])
+    targets = transformation(targets)
+    predictions = transformation(predictions)
+    print('targets:', targets)
+    print('predictions:', predictions)
+    print('wer:', jiwer.wer(targets, predictions))
